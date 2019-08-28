ab0e14c5190c5f795bf1f3e6f83cb8eedf64f673
Jorge Betancur <jabetancur12@gmail.com>
2019-08-27 21:37:44 -0500

    Listo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+let guests = [];
+let initialState = {firstName: "", lastName: ""}
 
 class App extends Component {
+  state = initialState
+
+  _handleSubmit = e => {
+    e.preventDefault();
+    guests = [...guests, this.state];
+    this.setState(initialState);
+  };
+
+  _handleChange = e => {
+    const name = e.target.name === "first-name" ? "firstName" : "lastName";
+    this.setState({
+      [name]: e.target.value
+    });
+  };
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this._handleSubmit}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={this._handleChange}
+                  name="first-name"
+                  value={this.state.firstName}
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="last-name"
+                  onChange={this._handleChange}
+                  value={this.state.lastName}
+                />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button type="submit" className="btn btn-primary">
+                  Agregar Invitado
+                </button>
               </div>
             </form>
 
@@ -30,16 +62,19 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {guests.map((guest, i) => (
+                  <tr key={i}>
+                    <td>{guest["firstName"]}</td>
+                    <td>{guest["lastName"]}</td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
+export default App;
